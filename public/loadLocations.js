2430ab4f2ff979b6242bb9e1c25836cbb391d554
Bruce Holland <bruholland@gmail.com>
2025-03-22 23:05:28 -0400

    hi

--- public/loadLocations.js
+++ public/loadLocations.js
@@ -1,4 +1,39 @@
-import { locations } from './locations.js'; // Import the locations array
+// import { locations } from './locations.js'; // Import the locations array
+const locations =
+    [
+        {
+            "name": "Chester, SC",
+            "address": "2355 Great Falls Hwy.",
+            "dates": ["2025-03-19", "2025-03-20"], 
+            "link": "https://purchase-tickets-forthe-kingdom-of-wonders.square.site/shop/319-320-chester-sc/C2KJR2W7VERKIHW3ULB2QDFY"
+        },
+        {
+            "name": "Lake City, SC",
+            "address": "748 McCutcheon Rd.",
+            "dates": ["2025-03-21", "2025-03-23"],
+            "link": "https://purchase-tickets-forthe-kingdom-of-wonders.square.site/shop/321-323-lake-city-sc/PHS5KRPI2PLN7UXVQ3QDQOS2"
+        },
+        {
+            "name": "Orangeburg, SC",
+            "address": "350 Magnolia Street",
+            "dates": ["2025-03-26", "2025-03-28"],
+            "link": "https://purchase-tickets-forthe-kingdom-of-wonders.square.site/shop/326-328-orangeburg-sc/3RFFSBJLOIBROIOHGFK57JY3"
+        },
+        {
+            "name": "Barnwell, SC",
+            "address": "12403 Ellenton Street",
+            "dates": ["2025-03-29", "2025-03-30"],
+            "link": "https://purchase-tickets-forthe-kingdom-of-wonders.square.site/shop/329-330-barnwell-sc/2I75PKPN4XSI7VRQKXCR6U7K"
+        },
+        {
+            "name": "Kinston, NC",
+            "address": "401 Fairgrounds Rd.",
+            "dates": ["2025-04-03", "2025-04-06"],
+            "link": "https://purchase-tickets-forthe-kingdom-of-wonders.square.site/shop/43-46-kinston-nc/LFYJX536FW2WNCAHST3VGP3K"
+        }
+    ];
+
+
 
 const locationsContainer = document.querySelector('.locations'); // Get the element with the class "locations"
 
