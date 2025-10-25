d67761f2a64a35c53513c465517488203e4a819f
Bruce Holland <bruholland@gmail.com>
2025-10-25 15:20:46 -0400

    Language for remaining Dry Ridge tickets

--- public/loadLocations.js
+++ public/loadLocations.js
@@ -123,7 +123,7 @@ load_locations().then(data => {
                 promo.innerHTML = "Last Show… Today<br>Don't Miss — Buy Now!";
             } else {
                 // promo.innerHTML = `⏳ In ${location.shows.length} Days… We're Gone.<br>Get tickets Now!`;
-                promo.innerHTML = `😊 Thank You Dry Ridge!`;
+                promo.innerHTML = `😊 Thank You Dry Ridge!<br>Tickets are almost gone.<br>Hurry, Reserve NOW!`;
             }
             locationDiv.appendChild(promo);
         } else {
